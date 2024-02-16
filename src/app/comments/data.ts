4dce9ce041fbe52a86b48794bbb3caac9eea2d44
Viveksingh99 <vivek22jan99@gmail.com>
2024-02-17 00:34:47 +0530

    comple routing

---
+++ src/app/comments/data.ts
@@ -0,0 +1,30 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is first comment",
+  },
+  {
+    id: 2,
+    text: "This is second comment",
+  },
+  {
+    id: 3,
+    text: "This is third comment",
+  },
+  {
+    id: 4,
+    text: "This is four comment",
+  },
+  {
+    id: 5,
+    text: "This is fiveth comment",
+  },
+  {
+    id: 6,
+    text: "This is sixth comment",
+  },
+  {
+    id: 7,
+    text: "This is siven comment",
+  },
+];
